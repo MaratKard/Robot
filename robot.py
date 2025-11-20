6bc5235208fa2e041873d5904a0e1b1d77395cef
MaratKard <m.t.kardanov@gmail.com>
2025-11-20 16:30:08 +0300

    Finished _on_trade and _on_order callback functions

--- robot.py
+++ robot.py
@@ -10,24 +10,22 @@ class_code = "QJSIM"
 sec_code = "SBER"
 
 
-orders_df = pd.DataFrame(columns=["Order_ID", "Security_Code", "Order_Type", "Placement_Date_Time", "Expiry_Date", "Price"])
-trades_df = pd.DataFrame(columns=["Trade_ID", "Order_ID", "Security_Code", "Trade_Type", "Execution_Date_Time", "Price"])
 
 def _on_order(data):
     """Creation of new order/change of existing order handler"""
-    print('OnOrder')
-    orders_df = load_orders(orders_df)
-    trades_df = load_trades(trades_df)
-    print(tabulate(orders_df, headers="keys", tablefmt="psql"))
-    print(tabulate(trades_df, headers="keys", tablefmt="psql"))
+    new_orders_df = load_orders()
+    print(tabulate(new_orders_df, headers="keys", tablefmt="psql"))
+    
+    #new_trades_df = load_trades()
+    #print(tabulate(new_trades_df, headers="keys", tablefmt="psql"))
 
 def _on_trade(data):
-    """ ??? """
-    print('OnTrade')
-    orders_df = load_orders(orders_df)
-    trades_df = load_trades(trades_df)
-    print(tabulate(orders_df, headers="keys", tablefmt="psql"))
-    print(tabulate(trades_df, headers="keys", tablefmt="psql"))
+    """ Execution of a new trade handler """
+    #new_orders_df = load_orders()
+    #print(tabulate(new_orders_df, headers="keys", tablefmt="psql"))
+    
+    new_trades_df = load_trades()
+    print(tabulate(new_trades_df, headers="keys", tablefmt="psql"))
        
     
 
@@ -61,36 +59,36 @@ def read_grid(sec_code) -> pd.DataFrame:
     grid["Quantity_delta_buy"] = grid["Quantity"] - grid["Quantity"].shift(1).fillna(grid.head(1)["Quantity"] - 1)
     return grid
 
-def load_orders(orders_df) -> pd.DataFrame:
+def load_orders() -> pd.DataFrame:
     curr_orders = qp_provider.get_all_orders()["data"]
+    new_orders_df = pd.DataFrame(columns=["Order_ID", "Security_Code", "Order_Type", "Placement_Date_Time", "Expiry_Date", "Price"])
     for curr_order in curr_orders:
-        if curr_order["balance"] != 0.0 and bin(curr_order["flags"])[2:][-2] != "1":
+        if curr_order["balance"] != 0.0 and format(curr_order["flags"], "#010b")[2:][-2] != "1":
             curr_order_row = pd.DataFrame({"Order_ID" : int(curr_order["ordernum"]), 
                                            "Security_Code" : curr_order["seccode"],
-                                           "Order_Type" : "Buy" if bin(curr_order["flags"])[2:][-3] == "0" else "Sell",
+                                           "Order_Type" : "Buy" if format(curr_order["flags"], "#010b")[2:][-3] == "0" else "Sell",
                                            "Placement_Date_Time" : f"{curr_order["datetime"]["day"]}.{curr_order["datetime"]["month"]}.{curr_order["datetime"]["year"]} {curr_order["datetime"]["hour"]}:{curr_order["datetime"]["min"]}:{curr_order["datetime"]["sec"]}",
                                            "Expiry_Date" : curr_order["expiry"],
                                            "Price" : curr_order["value"] / curr_order["qty"]}, index=[0])
-            if curr_order["ordernum"] not in orders_df["Order_ID"]:
-                with warnings.catch_warnings(category=FutureWarning):
-                    warnings.simplefilter(action="ignore")
-                    orders_df = pd.concat([orders_df, curr_order_row], axis=0, ignore_index=True)
-            return orders_df
+            with warnings.catch_warnings(category=FutureWarning):
+                warnings.simplefilter(action="ignore")
+                new_orders_df = pd.concat([new_orders_df, curr_order_row], axis=0, ignore_index=True)
+    return new_orders_df
 
-def load_trades(trades_df) -> pd.DataFrame:
+def load_trades() -> pd.DataFrame:
     curr_trades = qp_provider.get_all_trades()["data"]
+    new_trades_df = pd.DataFrame(columns=["Trade_ID", "Order_ID", "Security_Code", "Trade_Type", "Execution_Date_Time", "Price"])
     for curr_trade in curr_trades:
         curr_trade_row = pd.DataFrame({"Trade_ID" : curr_trade["trade_num"],
                                        "Order_ID" : curr_trade["ordernum"],
                                        "Security_Code" : curr_trade["seccode"],
-                                       "Trade_Type" : "Buy" if bin(curr_trade["flags"])[2:][-2] == "0" else "Sell",
+                                       "Trade_Type" : "Buy" if format(curr_trade["flags"], "#010b")[2:][-3] == "0" else "Sell",
                                        "Execution_Date_Time" : f"{curr_trade["datetime"]["day"]}.{curr_trade["datetime"]["month"]}.{curr_trade["datetime"]["year"]} {curr_trade["datetime"]["hour"]}:{curr_trade["datetime"]["min"]}:{curr_trade["datetime"]["sec"]}",
                                        "Price" : curr_trade["price"]}, index=[0])
-        if curr_trade["trade_num"] not in trades_df["Trade_ID"]:
-            with warnings.catch_warnings(category=FutureWarning):
-                warnings.simplefilter(action="ignore")
-                trades_df = pd.concat([trades_df, curr_trade_row], axis=0, ignore_index=True)
-        return trades_df
+        with warnings.catch_warnings(category=FutureWarning):
+            warnings.simplefilter(action="ignore")
+            new_trades_df = pd.concat([new_trades_df, curr_trade_row], axis=0, ignore_index=True)
+    return new_trades_df
 
 
 
